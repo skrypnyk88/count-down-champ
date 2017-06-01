e1b67fc1c3acd3c68602f8b489e2f3720f2a20ea
Dmytro Skrypnyk <skrypnyk_d@ukr.net>
2017-06-01 12:55:01 +0300

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import Clock from './Clock';
+import './App.css';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
+  constructor(prop) {
+    super(prop);
+    this.state = {
+      deadline: 'October 13, 2017',
+      newDedline: ''
+    }
+  }
+
+  changeDedline() {
+    this.setState({ deadline: this.state.newDedline })
+  }
+
   render() {
     return (
-      <div>Count Down Champ</div>
+      <div className="App">
+        <div className="App-title">Count to {this.state.deadline}</div>
+          <Clock deadline={this.state.deadline}/>
+          <Form inline>
+            <FormControl type="text"
+                         className="Deadline-input"
+                         placeholder="Enter date"
+                         onChange={event => {this.setState({newDedline: event.target.value})}}/>
+            <Button onClick={() => this.changeDedline()}>Submit</Button>
+          </Form>
+      </div>
     )
   }
 }
